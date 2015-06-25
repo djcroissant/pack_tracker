6e596f0df6e944837f42cf7fcee959972c16a664
Derek Covey <derek.covey@gmail.com>
2015-06-24 20:36:03 -0700

    transfer from inventory to packed items is fully functional

--- app/controllers/packer_controller.rb
+++ app/controllers/packer_controller.rb
@@ -5,15 +5,17 @@ class PackerController < ApplicationController
   end
 
   def pack_it
-    @params = params.select {|k,v| k =~ /^\d+/}
-    @params.each do |k,v|
-      ii = InventoryItem.find(k.to_i)
+    @params = params.select {|inventory_item_id,climber_name| inventory_item_id =~ /^\d+/}
+    @params.reject! {|inventory_item_id,climber_name| climber_name == "Not Packed"}
+    @params.each do |inventory_item_id,climber_name|
+      ii = InventoryItem.find(inventory_item_id)
       PackedItem.create(
-        climber_id: (Climber.find_by name: v).id,
+        climber_id: (Climber.find_by name: climber_name).id,
         title: ii.title,
         description: ii.description,
         weight: ii.weight,
-        group_item: ii.group_item)
+        group_item: ii.group_item
+        )
       end
     #redirect_to packer_allocate_path , alert: "#{params}"
   end
