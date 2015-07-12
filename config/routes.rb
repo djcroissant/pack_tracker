c5385b2b173b7e02977691bdb6bf718758e3fd7f
Derek Covey <derek.covey@gmail.com>
2015-07-11 23:01:11 -0700

    changed root path to welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'inventory_items#index'
+   root 'packer#welcome'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
