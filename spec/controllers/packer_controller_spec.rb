b1b2d4e68835664ae8a9f272a478bfc359503f6f
Derek Covey <derek.covey@gmail.com>
2015-08-09 23:01:03 -0700

    made tests for PackerController#pack_it

--- spec/controllers/packer_controller_spec.rb
+++ spec/controllers/packer_controller_spec.rb
@@ -52,10 +52,52 @@ describe PackerController do
       end
 
       it "assigns items with items for signed in user that are allocated to that expedition" do
+        #note that user.expeditions.first.inventory_items gives all inventory
+        #items for that expedition, not just those for the user.  But that's
+        #actually what we want here.
+        #But shoule make multiple users to be a better test
         expect(assigns(:items)).to eq user.expeditions.first.inventory_items
       end
     end
   end
 
+describe "#pack_it" do
+  context "when not signed in" do
+    it "redirects to sign in page" do
+      get :pack_it
+      expect(response).to redirect_to(login_url)
+    end
+  end
+
+  context "when signed in" do
+    before do
+      sign_in user
+    end
+
+    it "modifies inventory items in user's expedition based on user's selection" do
+      packed_items = user.expeditions.first.inventory_items.map do |i|
+        #makes it random whether a piece of inventory is "selected"
+        if rand(2) == 0
+          {i.id.to_s => "value" }
+        end
+      end
+      packed_items.compact!
+
+      if packed_items.empty?
+        packed_params = Hash.new
+        packed_ids = Array.new
+      else
+        packed_params = packed_items.reduce Hash.new, :merge
+        packed_ids = packed_params.keys.map { |k| k.to_i }
+      end
+
+      packed_params[:expedition_id] = user.expeditions.first.id
+      get :pack_it, packed_params
+
+
+      expect(assigns(:user).expeditions.first.inventory_items.where(user_id: user.id).map { |i| i.id }).to eq packed_ids
+    end
+  end
+end
 
 end
