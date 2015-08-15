bc435b72dc6f11dcd6cd8527ccc2a8a082f1e582
Derek Covey <derek.covey@gmail.com>
2015-08-15 11:20:39 -0700

    cleaned up code in ExpeditionMailer

--- app/mailers/expedition_mailer.rb
+++ app/mailers/expedition_mailer.rb
@@ -5,19 +5,20 @@ class ExpeditionMailer < ApplicationMailer
     @expedition = expedition
     @recipients = @expedition.users
     @recipients.each do |recipient|
-      send_list(recipient).deliver
+      send_list(recipient).deliver_now
     end
   end
 
   def send_list(recipient)
-    @items = []
-    @expedition.inventory_items.each do |inventory_item|
-      if inventory_item.user_id == recipient.id
-        @items << inventory_item
-        puts "@items.last = #{@items.last}"
-      end 
-    end
-    @items.sort!
+    @items = @expedition.inventory_items.where(user_id: recipient.id).order("title")
+    # @items = []
+    # @expedition.inventory_items.each do |inventory_item|
+    #   if inventory_item.user_id == recipient.id
+    #     @items << inventory_item
+    #     puts "@items.last = #{@items.last}"
+    #   end
+    # end
+    # @items.sort!
     mail(to: recipient.email, subject: 'test list email')
   end
 
