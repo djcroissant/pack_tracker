e200d5652b9604167bc584db971d01e01909fef4
Derek Covey <derek.covey@gmail.com>
2015-08-21 18:43:53 -0700

    update production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,9 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   # Use Gmail for outbound email
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'https://pack-tracker.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: "587",
